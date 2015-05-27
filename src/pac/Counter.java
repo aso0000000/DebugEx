73a0573f3d6ba69240f8f54553f7f1281e0fef4c
imamura <imamura@MIMAMURA.asojuku.local>
2015-05-27 19:16:17 +0900

    練習２

---
+++ src/pac/Counter.java
@@ -0,0 +1,15 @@
+package pac;
+/**
+ * 整数の演算を行うクラス
+ * JUnitを確認する
+ * @author imamura
+ *
+ */
+public class Counter {
+	public int add(int num1, int num2){
+		return num1+num2;
+	}
+	public int subtract(int num1, int num2){
+		return num2-num1;
+	}
+}
